9c2f95b430912758c7995673d954c5c1664e0276
Nikipikiganteng <nicholassuharto@gmail.com>
2022-11-27 19:50:28 +0700

    UGD SEMENTARA

--- app/src/main/java/com/example/ugd3_c_kel9/HomeActivity.kt
+++ app/src/main/java/com/example/ugd3_c_kel9/HomeActivity.kt
@@ -8,13 +8,13 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.ugd3_c_kel9.fragment.DaftarPaketFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)

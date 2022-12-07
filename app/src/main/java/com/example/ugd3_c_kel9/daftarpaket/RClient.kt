35a9e648b240cbf3af4f79ffe0780a526550aba7
Nikipikiganteng <nicholassuharto@gmail.com>
2022-12-07 23:30:03 +0700

    test

--- app/src/main/java/com/example/ugd3_c_kel9/daftarpaket/RClient.kt
+++ app/src/main/java/com/example/ugd3_c_kel9/daftarpaket/RClient.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RClient {
-    private const val BASE_URL = "http://192.168.100.154/ci4-apiserver-studeer/public/"
+    private const val BASE_URL = "http://4192.168.17.80ci4-apiserver-studeer/public/"
     val instances: api by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
